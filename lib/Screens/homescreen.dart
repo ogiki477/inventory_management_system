217186c45749a9228d266c4987af54428790f809
ogiki477 <omojick88moses@gmail.com>
2024-06-13 16:37:07 +0300

    homescreen

---
+++ lib/Screens/homescreen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  @override
+  build(context) {
+    return const Text("This is the HomeScreen");
+  }
+}
